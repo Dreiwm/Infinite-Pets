408e2f3e8071036cd905a0b25e2c6c58b85e7eef
Chris <Chris@DESKTOP-5UICP50>
2021-02-15 18:04:35 -0700

    Created add a pet servlet Started addAPetServlet

---
+++ Infinite Pets/src/java/servlets/addAPetServlet.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Chris
+ */
+public class addAPetServlet extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+                HttpSession session = request.getSession();
+                
+                getServletContext().getRequestDispatcher("/WEB-INF/addAPet.jsp").forward(request,response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       HttpSession session = request.getSession();
+        
+       String petName = request.getParameter("petName");
+       String sex = request.getParameter("sex");
+       String type = request.getParameter("animal");
+       String breed = request.getParameter("breed");
+       String birthday = request.getParameter("birthday");
+       String info = request.getParameter("info");
+       
+    }
+
+
+}
