3b9d5c1ef560f5b146a5e69b1c611cb2adfd744e
Chris <Chris@DESKTOP-5UICP50>
2021-02-21 22:52:41 -0700

    Roughed in AddPetServices put in functions to be done for the pet service

--- Infinite Pets/src/java/services/AddPetServices.java
+++ Infinite Pets/src/java/services/AddPetServices.java
@@ -5,10 +5,27 @@
  */
 package services;
 
+import java.util.List;
+import models.Accounts;
+import models.Pets;
+
 /**
  *
- * @author BTran
+ * @author BTran, Chris
  */
 public class AddPetServices {
+    
+    public List getAnimals(){
+        return null;
+    }
+   
+    public List getBreeds(String animal){
+        return null;
+    }
+    
+    public void setPet(Pets newPet, Accounts owner){
+        
+    }
+    
     
 }
