05eda505e931f8fbd39f90eb84794ddae937e923
Chris <Chris@DESKTOP-5UICP50>
2021-02-28 18:59:08 -0700

    Renamed files to meet new standards

--- Infinite Pets/src/java/servlets/petServlets/AddPetServlet.java
+++ Infinite Pets/src/java/servlets/petServlets/AddPetServlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package servlets;
+package servlets.petServlets;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
