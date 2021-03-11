0c0067eeeb1cf2a7e51fa7177c75bb9f1967b7d4
Riley <15681430+iCode2015@users.noreply.github.com>
2021-03-11 16:11:39 -0700

    feat: Add PromotionDB
    Note that inserting the promotions functionality is not tested but they 
    should work.

--- Infinite Pets/src/java/dataaccess/PromotionDB.java
+++ Infinite Pets/src/java/dataaccess/PromotionDB.java
@@ -5,36 +5,32 @@
  */
 package dataaccess;
 
-import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
-import models.Discount;
 import models.Promotion;
-import models.Service;
 
 /**
- * Responsible for interacting with discount table in the database.
+ * Responsible for interacting Promotion table in the database.
  * @author Riley
  */
-public class DiscountDB {
-    
+public class PromotionDB {
     /**
-     * Inserts the Discount into the discount table in the database.
-     * @param disc Discount to be inserted into.
+     * Inserts the Discount into the promotion table in the database.
+     * @param p Promotion to be inserted into.
      * @return returns true if successfully inserted into.
      * @throws Exception if something went wrong with process of inserting into database.
      */
-    public boolean insert(Discount disc) throws Exception {
+    public boolean insert(Promotion p) throws Exception {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
        EntityTransaction tr = em.getTransaction();
         
         try {
             tr.begin();
-            em.persist(disc);
+            em.persist(p);
             tr.commit();
             return true;
         } catch (Exception e) {
@@ -42,7 +38,7 @@ public class DiscountDB {
             if (tr.isActive()) {
                 tr.rollback();
             }
-            Logger.getLogger(Discount.class.getName()).log(Level.SEVERE, "Cannot insert " + disc.toString(), e); 
+            Logger.getLogger(Promotion.class.getName()).log(Level.SEVERE, "Cannot insert " + p.toString(), e); 
 
         } finally {
             em.close();
@@ -51,24 +47,24 @@ public class DiscountDB {
     }
     
     /**
-     * Updates given Discount object in the database.
-     * @param disc the Discount object to be updated.
-     * @return true if Discount was successfully persisted.
+     * Updates given Promotion object in the database.
+     * @param p the Promotion object to be updated.
+     * @return true if Promotion was successfully persisted.
      * @throws Exception if something went wrong with process of updating the object in the database.
      */
-    public boolean update(Discount disc) throws Exception {
+    public boolean update(Promotion p) throws Exception {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
         EntityTransaction tr = em.getTransaction();
         
         try {
             tr.begin();
-            em.merge(disc);
+            em.merge(p);
             tr.commit();
             return true;
         } catch (Exception e) {
             if (tr.isActive())
                 tr.rollback();
-            Logger.getLogger(Discount.class.getName()).log(Level.SEVERE, "Cannot update " + disc.toString(), e); 
+            Logger.getLogger(Promotion.class.getName()).log(Level.SEVERE, "Cannot update " + p.toString(), e); 
         } finally {
             em.close();
         }
@@ -76,22 +72,22 @@ public class DiscountDB {
     }
     
     /**
-     * Delete a row with given Discount object from the database.
-     * @param disc the Discount object to be deleted from the database.
+     * Delete a row with given Promethean object from the database.
+     * @param p the Promethean object to be deleted from the database.
      * @return true if successfully removed.
      * @throws Exception if something went wrong with process of deleting ab object from database.
      */
-    public boolean delete(Discount disc) throws Exception {
+    public boolean delete(Promotion p) throws Exception {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
         EntityTransaction tr = em.getTransaction();
         try {
            tr.begin();
-           em.remove(em.merge(disc));
+           em.remove(em.merge(p));
            tr.commit();
        } catch (Exception e){
            if (tr.isActive())
                tr.rollback();
-            Logger.getLogger(Discount.class.getName()).log(Level.SEVERE, "Cannot delete " + disc.toString(), e); 
+            Logger.getLogger(Process.class.getName()).log(Level.SEVERE, "Cannot delete " + p.toString(), e); 
            
        }
        finally {
@@ -101,86 +97,69 @@ public class DiscountDB {
     }
     
     /**
-     * Returns the Discount object with given ID.
+     * Returns the Promotion object with given ID.
      * @param id the id to be used to access a specific row in the Discount table.
-     * @return returns the Discount object with given ID.
+     * @return returns the Promotion object with given ID.
      * @throws Exception if something went wrong with process of retrieving given ID from database.
      */
-    public Discount get(int id) throws Exception {
+    public Promotion get(int id) throws Exception {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
         
         try {
-            return em.find(Discount.class, id);
+            return em.find(Promotion.class, id);
         } finally {
             em.close();
         }
     }
     
     /**
-     * Returns List of all Discount objects
-     * @return the List of Discount objects from the discount table.
+     * Returns List of all Promotion objects
+     * @return the List of Promotion objects from the promotion table.
      * @throws Exception if something went wrong with the process of retrieving all Discounts from the database.
      */
-    public List<Discount> getAllDiscounts() throws Exception {
+    public List<Promotion> getAllPromotions() throws Exception {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
         
         try {
-            return em.createNamedQuery("Discount.findAll", Discount.class).getResultList();
+            return em.createNamedQuery("Promotion.findAll", Promotion.class).getResultList();
         } finally {
             em.close();
         }
     }
     
-    // test 
     public static void main(String[] args) throws Exception {
-        DiscountDB dDB = new DiscountDB();
+        PromotionDB pDB = new PromotionDB();
+        
+        Promotion pr0 = new Promotion(0, "Some cool Promo", "Some promo description", new Date(), new Date(), true);
+        Promotion pr1 = new Promotion(0, "zzzSome cool Promo", "Some promo description 1234", new Date(), new Date(), false);
         
         // test insert twice
-        Discount disc1 = new Discount(0, new BigDecimal(12.00), 'a');
-        Discount disc2 = new Discount(0, new BigDecimal(11.00), 'b');
-        
-        PromotionDB pDB = new PromotionDB();
-        ServiceDB sDB = new ServiceDB();
-        Promotion pr = pDB.get(2);
-        
-        Service s = sDB.getServiceById(1);
-        
-        disc1.setPromoID(pr);
-        disc1.setServiceID(s);
-        disc2.setPromoID(pr);
-        disc2.setServiceID(s);
-        
-        dDB.insert(disc1);
-        dDB.insert(disc2);
+//        pDB.insert(pr0);
+//        pDB.insert(pr1);
         
         
         // test get all
-        List<Discount> l = dDB.getAllDiscounts();
-        for (Discount d : l) {
-            System.out.println(d.getDiscountID() + " " + d.getDiscountType());
+        List<Promotion> l =  pDB.getAllPromotions();
+        for (Promotion p : l) {
+            System.out.println(p.getPromotionName() + " " + p.getPromoDescription());
         }
         
+        // test update (first item)
+        pr0 = pDB.get(1);
+        pr0.setPromoDescription("Updated description. Seemignly another yet useless description");
+        pDB.update(pr0);
         
-        // test get by id
-        System.out.println(dDB.get(2));
+        // test get newly updated item
+        System.out.println(pDB.get(1).getPromoDescription());
         
+        // Test delete promo
+        pDB.delete(pr1);
         
-        // test update
-        disc2 = dDB.get(2);
-        disc2.setDiscount(new BigDecimal(20.00));
-        dDB.update(disc2);
-
-        
-        // test get updated object
-        System.out.println(dDB.get(2));
-        
-        // test delete first item.
-        dDB.delete(disc1);
-        
-        // last test get all (should contain only one item)
-        l = dDB.getAllDiscounts();
-        for (Discount d : l) {
-            System.out.println(d.getDiscountID() + " " + d.getDiscountType());
+        // See result
+        l =  pDB.getAllPromotions();
+        for (Promotion p : l) {
+            System.out.println(p.getPromotionName() + " " + p.getPromoDescription());
         }
     }
+    
 }
