61449cc402a9508c80aefa08dc057e8c282c3fb5
Chris <Chris@DESKTOP-5UICP50>
2021-03-14 23:26:24 -0600

    Merge origin/Cashton into Chris

--- Infinite Pets/src/java/servlets/InfinitePetsServlets/ResetPasswordServlet.java
+++ Infinite Pets/src/java/servlets/InfinitePetsServlets/ResetPasswordServlet.java
@@ -14,33 +14,20 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author BTran
+ * @author Chris
  */
-public class SignUpServlet extends HttpServlet {
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+public class ResetPasswordServlet extends HttpServlet {
+   
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/WEB-INF/SignUp.jsp").forward(request,response);
+        request.getRequestDispatcher("/WEB-INF/ResetPassword.jsp").forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+ 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+       
     }
 }
